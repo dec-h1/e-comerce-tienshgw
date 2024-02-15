06197083ecf01c4ecc12614c38de66f347c5bacd
decch <doracallisaya21@gmail.com>
2024-02-15 11:25:41 -0500

    agregar añadir carrito y context

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,24 @@ import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
 import NavBar from "./components/NavBar/NavBar";
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Carrito from "./components/Carrito/Carrito";
+import { CartProvider } from "./context/CartContext";
 
 import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
+      <CartProvider>
       <NavBar />
       <Routes>
         <Route path="/" element={<ItemListContainer saludo="Bienvenidos a Tiens: ¡Deseándoles a todos un excelente fin de semana!" />} />
         <Route path="/categoria/:categoria" element={<ItemListContainer saludo="Bienvenidos a Tiens" />} />
         <Route path="/detalle/:idProducto" element={<ItemDetailContainer />} />
+        <Route path="/carrito" element={<Carrito />} />
         <Route path="*" element={ <div>Pagina no encontrada</div> } />
       </Routes>
+      </CartProvider>
     </BrowserRouter>
   );
 }
